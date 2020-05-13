4536ac793214de2392554d6f0f1e902afc285275
Pat Doyle <patrick.doyle126@gmail.com>
2020-05-13 16:37:17 -0500

    updating mappings and schema for options/pool contracts

--- generated/schema.ts
+++ generated/schema.ts
@@ -278,4 +278,263 @@ export class MintEventOption extends Entity {
   set outTokenR(value: BigInt) {
     this.set("outTokenR", Value.fromBigInt(value));
   }
+
+  get hash(): string {
+    let value = this.get("hash");
+    return value.toString();
+  }
+
+  set hash(value: string) {
+    this.set("hash", Value.fromString(value));
+  }
+}
+
+export class SwapEventOption extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save SwapEventOption entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save SwapEventOption entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("SwapEventOption", id.toString(), this);
+  }
+
+  static load(id: string): SwapEventOption | null {
+    return store.get("SwapEventOption", id) as SwapEventOption | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get from(): Bytes {
+    let value = this.get("from");
+    return value.toBytes();
+  }
+
+  set from(value: Bytes) {
+    this.set("from", Value.fromBytes(value));
+  }
+
+  get outTokenU(): BigInt {
+    let value = this.get("outTokenU");
+    return value.toBigInt();
+  }
+
+  set outTokenU(value: BigInt) {
+    this.set("outTokenU", Value.fromBigInt(value));
+  }
+
+  get inTokenS(): BigInt {
+    let value = this.get("inTokenS");
+    return value.toBigInt();
+  }
+
+  set inTokenS(value: BigInt) {
+    this.set("inTokenS", Value.fromBigInt(value));
+  }
+
+  get hash(): string {
+    let value = this.get("hash");
+    return value.toString();
+  }
+
+  set hash(value: string) {
+    this.set("hash", Value.fromString(value));
+  }
+}
+
+export class RedeemEventOption extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save RedeemEventOption entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save RedeemEventOption entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("RedeemEventOption", id.toString(), this);
+  }
+
+  static load(id: string): RedeemEventOption | null {
+    return store.get("RedeemEventOption", id) as RedeemEventOption | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get from(): Bytes {
+    let value = this.get("from");
+    return value.toBytes();
+  }
+
+  set from(value: Bytes) {
+    this.set("from", Value.fromBytes(value));
+  }
+
+  get inTokenR(): BigInt {
+    let value = this.get("inTokenR");
+    return value.toBigInt();
+  }
+
+  set inTokenR(value: BigInt) {
+    this.set("inTokenR", Value.fromBigInt(value));
+  }
+
+  get hash(): string {
+    let value = this.get("hash");
+    return value.toString();
+  }
+
+  set hash(value: string) {
+    this.set("hash", Value.fromString(value));
+  }
+}
+
+export class CloseEventOption extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save CloseEventOption entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save CloseEventOption entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("CloseEventOption", id.toString(), this);
+  }
+
+  static load(id: string): CloseEventOption | null {
+    return store.get("CloseEventOption", id) as CloseEventOption | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get from(): Bytes {
+    let value = this.get("from");
+    return value.toBytes();
+  }
+
+  set from(value: Bytes) {
+    this.set("from", Value.fromBytes(value));
+  }
+
+  get inTokenP(): BigInt {
+    let value = this.get("inTokenP");
+    return value.toBigInt();
+  }
+
+  set inTokenP(value: BigInt) {
+    this.set("inTokenP", Value.fromBigInt(value));
+  }
+
+  get hash(): string {
+    let value = this.get("hash");
+    return value.toString();
+  }
+
+  set hash(value: string) {
+    this.set("hash", Value.fromString(value));
+  }
+}
+
+export class FundEventOption extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id !== null, "Cannot save FundEventOption entity without an ID");
+    assert(
+      id.kind == ValueKind.STRING,
+      "Cannot save FundEventOption entity with non-string ID. " +
+        'Considering using .toHex() to convert the "id" to a string.'
+    );
+    store.set("FundEventOption", id.toString(), this);
+  }
+
+  static load(id: string): FundEventOption | null {
+    return store.get("FundEventOption", id) as FundEventOption | null;
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get cacheU(): BigInt {
+    let value = this.get("cacheU");
+    return value.toBigInt();
+  }
+
+  set cacheU(value: BigInt) {
+    this.set("cacheU", Value.fromBigInt(value));
+  }
+
+  get cacheS(): BigInt {
+    let value = this.get("cacheS");
+    return value.toBigInt();
+  }
+
+  set cacheS(value: BigInt) {
+    this.set("cacheS", Value.fromBigInt(value));
+  }
+
+  get cacheR(): BigInt {
+    let value = this.get("cacheR");
+    return value.toBigInt();
+  }
+
+  set cacheR(value: BigInt) {
+    this.set("cacheR", Value.fromBigInt(value));
+  }
+
+  get hash(): string {
+    let value = this.get("hash");
+    return value.toString();
+  }
+
+  set hash(value: string) {
+    this.set("hash", Value.fromString(value));
+  }
 }
--- src/mappingOption.ts
+++ src/mappingOption.ts
@@ -1,6 +1,18 @@
 import { BigInt } from "@graphprotocol/graph-ts";
-import { Mint } from "../generated/primeOption/primeOption";
-import { MintEventOption } from "../generated/schema";
+import {
+  Mint,
+  Swap,
+  Redeem,
+  Close,
+  Fund,
+} from "../generated/primeOption/primeOption";
+import {
+  MintEventOption,
+  SwapEventOption,
+  RedeemEventOption,
+  CloseEventOption,
+  FundEventOption,
+} from "../generated/schema";
 
 export function handleMint(event: Mint): void {
   let entity = new MintEventOption(
@@ -9,5 +21,48 @@ export function handleMint(event: Mint): void {
   entity.from = event.params.from;
   entity.outTokenP = event.params.outTokenP;
   entity.outTokenR = event.params.outTokenR;
+  entity.hash = event.transaction.hash.toHex();
+  entity.save();
+}
+
+export function handleSwap(event: Swap): void {
+  let entity = new SwapEventOption(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.from = event.params.from;
+  entity.outTokenU = event.params.outTokenU;
+  entity.inTokenS = event.params.inTokenS;
+  entity.hash = event.transaction.hash.toHex();
+  entity.save();
+}
+
+export function handleRedeem(event: Redeem): void {
+  let entity = new RedeemEventOption(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.from = event.params.from;
+  entity.inTokenR = event.params.inTokenR;
+  entity.hash = event.transaction.hash.toHex();
+  entity.save();
+}
+
+export function handleClose(event: Close): void {
+  let entity = new CloseEventOption(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.from = event.params.from;
+  entity.inTokenP = event.params.inTokenP;
+  entity.hash = event.transaction.hash.toHex();
+  entity.save();
+}
+
+export function handleFund(event: Fund): void {
+  let entity = new FundEventOption(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+  entity.cacheU = event.params.cacheU;
+  entity.cacheS = event.params.cacheS;
+  entity.cacheR = event.params.cacheR;
+  entity.hash = event.transaction.hash.toHex();
   entity.save();
 }
